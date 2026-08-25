903166e82341f1509ba0c6820aa5d8fbcb70b5be
渡辺 実咲 <watanabe.misaki@cyaas.co.jp>
2026-08-25 18:21:40 +0900

    fix leaflet icons_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import L from "leaflet";
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import markerIcon2x from "leaflet/dist/images/ma
+rker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const spots = [
   {
